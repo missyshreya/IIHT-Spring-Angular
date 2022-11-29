125d34f4c8366723a679937fe0baa30f48453f17
unknown <cogjava4576@iiht.tech>
2022-11-29 15:21:46 +0530

    added predicate interface with lambda demo

--- Java8-Basics/Java8-basics2/src/com/basics/functionalInterface/PredicateDemo.java
+++ Java8-Basics/Java8-basics2/src/com/basics/functionalInterface/PredicateDemo.java
@@ -1,22 +1,36 @@
 package com.basics.functionalInterface;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
 
-public class PredicateDemo implements Predicate<Integer> {
+
+
+public class PredicateDemo{
 	
-	@Override
-	public boolean test(Integer t) {
+	/*public static void main(String args[])
+	{
+		Predicate<Integer> predicate= (t)->{
+			if(t%2==0) {
+				return true;
+			}
+			return false;
+		};
 		
-		if(t%2==0) {
-			return true;
-		}
-		return false;
-		
-	}
+	    System.out.println(predicate.test(50));
+	}*/
+
+	
 	public static void main(String args[])
 	{
-		Predicate<Integer> predicate= new PredicateDemo();
-		System.out.println(predicate.test(5));
+		Predicate<Integer> predicate= (t)-> (t%2==0);
+		
+	    System.out.println(predicate.test(50));
+	    
+	    
+	    List<Integer> numberList = Arrays.asList(1,2,3,4,5);
+	    numberList.stream().filter(predicate).forEach(number-> System.out.println("printing: "+ number));
 	}
-
+	
+	
 }
