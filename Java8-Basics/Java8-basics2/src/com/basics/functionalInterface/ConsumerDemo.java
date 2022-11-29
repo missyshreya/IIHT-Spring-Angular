c863343857d0a102d2b3b789213d021604c43062
unknown <cogjava4576@iiht.tech>
2022-11-29 14:51:05 +0530

    added consumer interface demo

---
+++ Java8-Basics/Java8-basics2/src/com/basics/functionalInterface/ConsumerDemo.java
@@ -0,0 +1,24 @@
+package com.basics.functionalInterface;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+	
+	public static void main(String args[])
+	{
+		//Consumer<Integer> consumer=(t) -> 
+		//	System.out.println("printing : "+t);
+		//	consumer.accept(4000);
+			
+			
+		List<Integer> numberList = Arrays.asList(1,2,3,4,5);
+		
+		//numberList.stream().forEach(consumer);
+		
+		numberList.stream().forEach(number-> System.out.println("printing: "+number));
+		
+		
+	}
+	
+}
