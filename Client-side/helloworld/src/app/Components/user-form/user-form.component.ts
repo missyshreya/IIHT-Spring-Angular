e203212dbeb1d172ebb14d5b76b8fbf6fb96e430
unknown <cogjava4576@iiht.tech>
2022-12-15 17:43:12 +0530

    added post method

--- Client-side/helloworld/src/app/Components/user-form/user-form.component.ts
+++ Client-side/helloworld/src/app/Components/user-form/user-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import User from 'src/app/Entity/user';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-user-form',
@@ -11,16 +12,19 @@ export class UserFormComponent implements OnInit {
   title: String= 'Please fill the form below';
 
 user: User= new User();
-
-
   save(){
-    console.log('Hello '+this.user.firstname+' '+this.user.lastname);
-    console.log('your email add is '+this.user.email);
+    const observables= this.userService.saveUser(this.user);
+    observables.subscribe(
+      (response: any) =>{
+        console.log(response);
+      }, function(error){
+        console.log(error);
+        alert("something went wrong, Please try again!")
+      }
+    )    
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {}
 }
+
+
