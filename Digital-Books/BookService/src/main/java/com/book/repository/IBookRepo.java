69d19619a59bff637da8dd48bb560c9bb0c8014e
Shreya <98869562+missyshreya@users.noreply.github.com>
2022-12-22 12:28:16 +0530

    Delete Digital-Books directory

--- Digital-Books/BookService/src/main/java/com/book/repository/IBookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.book.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.book.entity.Books;
-
-public interface IBookRepo extends JpaRepository<Books, Integer> {
-
-}
