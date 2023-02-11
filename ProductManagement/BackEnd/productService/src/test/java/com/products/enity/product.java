d6c96bccfabac8070bf7371a150c4407de26c860
unknown <cogjava4576@iiht.tech>
2023-02-12 01:15:27 +0530

    updating

--- ProductManagement/BackEnd/productService/src/test/java/com/products/enity/product.java
+++ ProductManagement/BackEnd/productService/src/test/java/com/products/enity/product.java
@@ -1,25 +1,31 @@
 package com.products.enity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 
+@Entity
+@Table(name = "product")
 @Data
-
 public class product {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "details")
 	private String details;
+	
+	@Column(name = "price")
 	private int price;
 }
